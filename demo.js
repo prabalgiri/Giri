5fbfd05de33484724636c0284f34d0ad65b84c19
prabalgiri <92464818+prabalgiri@users.noreply.github.com>
2023-06-24 15:46:46 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,5 +3,7 @@ console.log(data);
 data.style.backgroundColor="green";
 let element=document.getElementsByTagName("li:last-chile");
 console.log(element);
+let data2=document.getelementByClassName("list-group-item")[1];
+data2.style.backgroundColor="green";
 
 

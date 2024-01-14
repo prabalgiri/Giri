0fca7c30dd6ee5acf40f782a785d1c1998df7060
prabalgiri <92464818+prabalgiri@users.noreply.github.com>
2024-01-14 20:49:18 +0530

    Update addButton.js

--- addButton.js
+++ addButton.js
@@ -1,13 +1,49 @@
-let button =document.createElement(<button type="button">Delete</button>);
-divset.appendChild(button);
-button.addEventListener("click",(e)=>{
-  divSet.deleteChild(li);
-})
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-let editButton =document.createChild(<button type="button">Edit</button>);
-divset.appendChild(editButton);
-editButton.addEventListener("click",(e)=>{
-  emailId.innerText=localStorege.get(id);
-  password.innerText=localStorage.get(pas);
-  name.innerText=localStorege.get(name);
-})
+const app = express();
+const port = 3000;
+
+// MySQL database connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'ROOT',
+  password: 'root',
+  database: 'productsdb'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
+// Express middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Route to display products
+app.get('/', (req, res) => {
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) throw err;
+    res.render('index', { products: results });
+  });
+});
+
+// Route to delete a product
+app.post('/delete/:id', (req, res) => {
+  const productId = req.params.id;
+
+  db.query('DELETE FROM products WHERE id = ?', [productId], (err, result) => {
+    if (err) throw err;
+    console.log(`Deleted ${result.affectedRows} product(s)`);
+    res.redirect('/');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

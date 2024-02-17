33cec07fed6b5596f166d17e04071498e6e9f334
prabalgiri <92464818+prabalgiri@users.noreply.github.com>
2024-02-17 17:42:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-function login(event){
-    event.preventDefault();
-    console.log(event.target.name);
-
-    const loginData={
-        email: event.target.value,
-        password:event.target.password.value
-    }
-    
-     console.log(loginData);
-     axios.post('http://localhost:3000/user/login',loginData).then(response=>{
-     }).catch(err=>{
-        console.log(JSON.stringify(err));
-        document.body.innerHTML+=`<div>${err.message}</div>`
-     })
-
-
+const login=(req,res)=>{
+    const {email,password}=req.body;
+    console.log(req.body);
+    User.findAll({where:{ email}}).then(user=>{
+        if(user.length>0){
+            if(user[0].password===password){
+                res.status(200).json({success:true,message:"user Loged In"})
+            }else{
+                return res.status(400).json({success:false,message:'Wrong Password'})
+            }
+        }else{
+            return res.status(404).json({success:false,message:'User not exists'})
+        }
+    }).catch(err=>{
+        res.status(500).json({message:err,success:false})
+    })
 }
